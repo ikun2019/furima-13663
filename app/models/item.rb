b7e4e3c238175e0fbeb3c998a4136385f8823fd7
ikun <isao.miyagawa.2019@gmail.com>
2021-01-17 00:23:54 +0900

    バリデーションと商品出品ページを表示できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :sending_area
+  belongs_to :sending_day
+
+
+  with_options presence :true do
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :sending_area_id, numericality: { other_than: 1 }
+    validates :sending_days_id, numericality: { other_than: 1 }
+  end
+
 end
