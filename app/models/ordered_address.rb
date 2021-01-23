74d9ba232899a9b3b45c5abde1e3d87e8d57b923
ikun <isao.miyagawa.2019@gmail.com>
2021-01-23 19:01:16 +0900

    購入機能の実装（単体テスト前）

---
+++ app/models/ordered_address.rb
@@ -0,0 +1,18 @@
+class OrderedAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :order_id, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+  end
+end
---
+++ spec/factories/ordered_addresses.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ordered_address do
+    
+  end
+end
