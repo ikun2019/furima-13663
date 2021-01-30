fa877a3f273513733c34435585b469ca8b02b9f0
ikun <isao.miyagawa.2019@gmail.com>
2021-01-30 23:43:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/furima.pem']
+set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/furima.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
